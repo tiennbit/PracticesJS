e38dc7dc12335b3142eb1fa6d82594d0014e15cd
tiennbit <tiennbit@gmail.com>
2022-11-23 20:27:11 +0700

    Bai 2 buoi 2

---
+++ lesson-2/bai2.js
@@ -0,0 +1,9 @@
+function compareAge(name1, name2, age1, age2){
+    var olderPerson =  age1 > age2 ? name1 : name2;
+    var youngerPerson =  age1 < age2 ? name2 : name1;
+
+    console.log(olderPerson);
+    console.log(`Nguoi lon tuoi hon la: ${olderPerson}. Nguoi nho tuoi hon la: ${youngerPerson}`);
+}
+
+compareAge("Tien", "Binh", 19, 15);
