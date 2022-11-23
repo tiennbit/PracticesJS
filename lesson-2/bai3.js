7d253919a4aaab8ae59ba38694121a68efa6c5d0
tiennbit <tiennbit@gmail.com>
2022-11-23 20:36:05 +0700

    bai 3 buoi 3

---
+++ lesson-2/bai3.js
@@ -0,0 +1,30 @@
+function findDay(day) {
+    switch (day){
+        case 2:
+            console.log("Thu 2");
+            break;
+        case 3:
+            console.log("Thu 3");
+            break;
+        case 4:
+            console.log( "Thu 4");
+            break;
+        case 5:
+            console.log("Thu 5");
+            break;
+        case 6:
+            console.log( "Thu 6");
+            break;
+        case 7:
+            console.log( "Thu 7");
+            break;
+        case 8:
+            console.log( "Chu nhat");
+            break;
+        default:
+            console.log("Khong phai ngay trong tuan")
+            break;
+    }
+} 
+
+findDay(10);
